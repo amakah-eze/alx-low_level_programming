ad7a9c5c18d19e1eb829174a9097b87aa0334877
Eze Chiamaka <ezechiamaka960@gmail.com>
2022-10-16 02:42:29 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main- serves as the entry point for the program
  *
- * Return: By default return zero if no errors otherwise returns non-zero value.
+ * Return: By default return zero if no errors otherwise return non-zero value.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
